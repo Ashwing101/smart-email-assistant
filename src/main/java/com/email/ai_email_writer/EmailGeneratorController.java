7c29a24e468bd80a054729ffabae5f411284f6c4
Ashwini <gourashwini115@gmail.com>
2025-06-09 13:08:13 -0400

    Added CORS to accept request from all

--- src/main/java/com/email/ai_email_writer/EmailGeneratorController.java
+++ src/main/java/com/email/ai_email_writer/EmailGeneratorController.java
@@ -3,14 +3,14 @@ package com.email.ai_email_writer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.lang.model.util.Elements;
 
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private EmailGeneratorService emailGeneratorService;
